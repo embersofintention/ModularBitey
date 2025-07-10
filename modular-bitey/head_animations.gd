fc4d960e96a364d8bd2ba60d434ac08d7dadeb8a
embersofintention <embersofintention@pm.me>
2025-07-10 13:39:09 -0400

    Adding the game files to the repo

---
+++ modular-bitey/head_animations.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+
+# variables
+var current_animation := "" # for animation helper function
+var this_node = ""
+
+func _ready() -> void:
+	# return to other stuff
+	%AnimationPlayer.connect("animation_finished", Callable(self, "_on_animation_finished"))
+	# set this_node to node's name
+	this_node == name
+	
+
+# ANIMATION HELPER (not sure if working yet?)
+func _on_animation_finished(anim_name):
+	if current_animation == anim_name:
+		current_animation = ""
+
+
+# HELPER FUNCTION:  play animation only if different
+func change_animation(anim_name: String):
+	if current_animation != anim_name: 
+		%AnimationPlayer.play(anim_name)
+		current_animation = anim_name
+
+
+func play_idle_animation(): 
+	# %AnimationPlayer.play("idle")
+	change_animation("idle2")
+
+
+func play_walk_animation():
+	# %AnimationPlayer.play("walk")
+	change_animation("run2")
+
+func play_bite_animation(): #NOT CURRENTLY WORKING! fix it later
+	# %AnimationPlayer.play("bite")
+	change_animation("bite")
+	print_debug("Bite!")
+	
+	
+func play_test_animation():
+	#%AnimationPlayer.play("TEST")
+	change_animation("TEST")
+	
+ #func _on_animation_finished(anim_name):
---
+++ modular-bitey/player.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+
+ #variables
+var Fella # for calling that node; defined in _ready
+var current_animation = "" 
+var player_scale = 5
+
+# constants
+const SPEED = 600
+
+
+
+func _ready() -> void:
+	# instance other nodes
+	Fella = %PlayerAnimations
+	# get_tree().debug_collisions_hint = true
+	# ! define helper functions later
+	
+# Helper Function -- Only change animations when needed, not every frame
+
+
+func _physics_process(delta: float) -> void:
+	
+			# Bite!
+	if Input.is_action_just_pressed("bite"):
+		Fella.play_bite_animation()
+	
+	# get direction based on player input
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	# set player velocity
+	velocity = direction * SPEED
+	
+	
+	# player faces direction
+	if direction:
+		# if moving horizontally
+		if velocity.x != 0 and velocity.y == 0:
+			# face the correct direction
+			Fella.scale.x = direction.x
+		# and keeping scale.y from getting weird
+		if velocity.y:
+			Fella.scale.y = 1
+	
+	# player looks towards mouse
+	# look_at(get_global_mouse_position())
+
+	
+	
+	# Animations
+	if velocity.length() > 0.0: 
+		Fella.play_walk_animation()
+	else: 
+		Fella.play_idle_animation()
+	
+
+		
+		
+	# apply movement
+	move_and_slide()
